67a27354a230ab675f4fbfe22d278c1554afb44e
Kinoti Mitchell <kinotimitchell@gmail.com>
2025-10-06 13:22:59 +0300

    Deploy to main branch

--- src/components/DisposalManagement.tsx
+++ src/components/DisposalManagement.tsx
@@ -222,7 +222,7 @@ const DisposalManagement: React.FC = () => {
         </div>
         
         <Dialog open={createDialogOpen} onOpenChange={setCreateDialogOpen}>
-            <DialogContent className="max-w-6xl w-[95vw] max-h-[95vh] flex flex-col">
+            <DialogContent className="max-w-6xl w-[95vw] h-[95vh] flex flex-col">
               <DialogHeader>
                 <DialogTitle className="text-xl font-semibold">Create New Disposal Record</DialogTitle>
                 <DialogDescription>
@@ -230,7 +230,7 @@ const DisposalManagement: React.FC = () => {
                 </DialogDescription>
               </DialogHeader>
               
-              <div className="space-y-4 flex-1 overflow-y-auto overflow-x-hidden">
+              <div className="space-y-4 flex-1 overflow-y-auto overflow-x-hidden min-h-0 max-h-full">
               {/* First Step - Select Filter Criteria */}
               <Card className="shadow-lg border-0 bg-gradient-to-br from-blue-50 to-indigo-50 max-w-md mx-auto">
                 <CardHeader className="pb-4">
